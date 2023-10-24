7f86f3a9fc3375a8184e5c3087f5a474821851b5
taesung1993 <cheonyulin@gmail.com>
2023-10-24 17:40:13 +0900

    display delete button when drag direction is right

--- lib/widgets/workout_item/workout_item_header.dart
+++ lib/widgets/workout_item/workout_item_header.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:jeomgymjeok_gwabuha/design/Pallete.dart';
 import 'package:jeomgymjeok_gwabuha/design/Types.dart';
 
-class WorkoutItemHeader extends StatelessWidget {
+class WorkoutItemHeader extends StatefulWidget {
   const WorkoutItemHeader({
     super.key,
     required this.onTapHeader,
@@ -17,65 +18,118 @@ class WorkoutItemHeader extends StatelessWidget {
   final int totalSetCount;
 
   @override
+  State<WorkoutItemHeader> createState() => _WorkoutItemHeaderState();
+}
+
+class _WorkoutItemHeaderState extends State<WorkoutItemHeader> {
+  bool isDisplayDeleteButton = false;
+  String direction = '';
+
+  @override
   Widget build(BuildContext context) {
     final double fullWidth = MediaQuery.of(context).size.width;
+    final double contentWidth =
+        isDisplayDeleteButton ? fullWidth - 80 : fullWidth;
 
-    return GestureDetector(
-      onTap: onTapHeader,
-      child: Container(
-        width: fullWidth,
-        height: 80,
-        color: pallete[Pallete.alaskanBlue],
-        child: Stack(children: [
+    return Container(
+      width: fullWidth,
+      height: 80,
+      child: Stack(
+        children: [
           Positioned(
             top: 0,
             left: 0,
-            child: Container(
+            child: GestureDetector(
+              onTap: widget.onTapHeader,
+              onHorizontalDragUpdate: (details) => {
+                if (details.delta.dx > 0)
+                  {direction = 'right'}
+                else
+                  {direction = 'left'}
+              },
+              onHorizontalDragEnd: (details) {
+                setState(() {
+                  isDisplayDeleteButton = direction == 'right';
+                });
+              },
+              child: Container(
                 width: fullWidth,
                 height: 80,
-                padding: const EdgeInsets.only(left: 52, right: 51),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                color: pallete[Pallete.alaskanBlue],
+                child: Stack(
                   children: [
-                    Text(
-                      name,
-                      style: types[Types.semi_lg]!.copyWith(
-                        color: pallete[Pallete.white],
-                      ),
+                    Positioned(
+                      left: 0,
+                      bottom: 0,
+                      child: AnimatedContainer(
+                          width: contentWidth,
+                          duration: const Duration(milliseconds: 150),
+                          curve: Curves.easeInOut,
+                          height: 80,
+                          padding: const EdgeInsets.only(left: 52, right: 51),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                widget.name,
+                                style: types[Types.semi_lg]!.copyWith(
+                                  color: pallete[Pallete.white],
+                                ),
+                              ),
+                              Text(
+                                '${widget.totalSetCount}Set',
+                                style: types[Types.semi_lg]!.copyWith(
+                                  color: pallete[Pallete.white],
+                                ),
+                              ),
+                            ],
+                          )),
                     ),
-                    Text(
-                      '${totalSetCount}Set',
-                      style: types[Types.semi_lg]!.copyWith(
-                        color: pallete[Pallete.white],
+                    if (!isDisplayDeleteButton)
+                      Positioned(
+                        left: 0,
+                        bottom: 8,
+                        child: Container(
+                          width: fullWidth,
+                          height: 2,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: AnimatedContainer(
+                            width: widget.isExpanded ? fullWidth : 82,
+                            height: 2,
+                            duration: const Duration(milliseconds: 150),
+                            curve: Curves.easeInOut,
+                            decoration: BoxDecoration(
+                              color: widget.isExpanded
+                                  ? pallete[Pallete.deepNavy]!.withOpacity(0.5)
+                                  : pallete[Pallete.white],
+                              borderRadius: BorderRadius.circular(9999),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
                   ],
-                )),
-          ),
-          Positioned(
-            left: 0,
-            bottom: 8,
-            child: Container(
-              width: fullWidth,
-              height: 2,
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: AnimatedContainer(
-                width: isExpanded ? fullWidth : 82,
-                height: 2,
-                duration: const Duration(milliseconds: 150),
-                curve: Curves.easeInOut,
-                decoration: BoxDecoration(
-                  color: isExpanded
-                      ? pallete[Pallete.deepNavy]!.withOpacity(0.5)
-                      : pallete[Pallete.white],
-                  borderRadius: BorderRadius.circular(9999),
                 ),
               ),
             ),
           ),
-        ]),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              curve: Curves.easeInOut,
+              width: isDisplayDeleteButton ? 80 : 0,
+              height: 80,
+              color: pallete[Pallete.red02],
+              child: IconButton(
+                icon: SvgPicture.asset('assets/icons/white_trash.svg'),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
