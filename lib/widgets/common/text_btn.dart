224a4b7fef4e92185098b7b5c59600fa70e73f82
taesung1993 <cheonyulin@gmail.com>
2023-10-26 18:12:29 +0900

    create text_btn widget

---
+++ lib/widgets/common/text_btn.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:jeomgymjeok_gwabuha/design/Pallete.dart';
+import 'package:jeomgymjeok_gwabuha/design/Types.dart';
+
+class TextBtn extends StatelessWidget {
+  const TextBtn({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.width = 120,
+    this.height = 56,
+    this.backgroundColor = Pallete.deepNavy,
+    this.textColor = Pallete.white,
+  });
+
+  final double? width;
+  final double? height;
+  final String text;
+
+  final Pallete? backgroundColor;
+  final Pallete? textColor;
+
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(0),
+          backgroundColor: pallete[backgroundColor],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: types[Types.semi_md]!.copyWith(
+            color: pallete[textColor],
+          ),
+        ),
+      ),
+    );
+  }
+}
