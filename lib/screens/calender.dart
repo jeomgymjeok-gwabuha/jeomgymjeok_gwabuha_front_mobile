ea98dbd518e08442cfd33f6c172c73c00c2f3267
Taesung Yun <cheonyulin@gmail.com>
2023-10-27 14:36:11 +0900

    publish edit bar in calendar-screen

--- lib/screens/calender.dart
+++ lib/screens/calender.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jeomgymjeok_gwabuha/data/dummy_workout_list.dart';
+import 'package:jeomgymjeok_gwabuha/design/Pallete.dart';
+import 'package:jeomgymjeok_gwabuha/design/Types.dart';
 import 'package:jeomgymjeok_gwabuha/models/m_workout_item.dart';
 import 'package:jeomgymjeok_gwabuha/widgets/workout_list.dart';
 
@@ -10,8 +12,30 @@ class CalendarScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<MWorkoutItem> workoutList = dummyWorkoutList;
 
-    return WorkoutList(
-      list: workoutList,
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 40,
+          color: pallete[Pallete.deepNavy],
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.symmetric(horizontal: 36),
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              '편집',
+              style: types[Types.semi_md]!.copyWith(
+                color: pallete[Pallete.white],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: WorkoutList(
+            list: workoutList,
+          ),
+        ),
+      ],
     );
   }
 }
