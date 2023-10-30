7373abe97a6fc42c7e907a7273bde6bcba110780
taesung1993 <cheonyulin@gmail.com>
2023-10-31 01:42:23 +0900

    change align column to stack

--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -25,191 +25,209 @@ class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
     final double fullWidth = MediaQuery.of(context).size.width;
-    // TODO: implement build
-    return Column(
+
+    return Stack(
       children: [
-        const SizedBox(height: 1),
-        Container(
-          width: fullWidth,
-          height: 40,
-          color: pallete[Pallete.deepNavy],
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 0,
-                child: SizedBox(
-                  width: fullWidth,
-                  height: 40,
-                  child: Center(
-                    child: Text(
-                      DateFormat('yyyy.MM.dd').format(_selectedDay),
-                      style: types[Types.semi_lg]!.copyWith(
-                        color: pallete[Pallete.white],
+        Positioned.fill(
+          child: Container(
+            color: Colors.transparent,
+          ),
+        ),
+        Positioned(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 57.31),
+            child: TableCalendar(
+              firstDay: DateTime(1800),
+              lastDay: DateTime(3000),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              daysOfWeekHeight: 50,
+              rowHeight: 50,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  _calendarFormat = CalendarFormat.week;
+                });
+              },
+              headerStyle: HeaderStyle(
+                titleCentered: true,
+                formatButtonVisible: false,
+                titleTextFormatter: (date, locale) {
+                  return DateFormat.MMMM(locale).format(date);
+                },
+                headerPadding: const EdgeInsets.all(0),
+                headerMargin: const EdgeInsets.only(bottom: 12),
+                titleTextStyle: types[Types.semi_lg]!.copyWith(
+                  color: pallete[Pallete.white],
+                ),
+                decoration: BoxDecoration(
+                  color: pallete[Pallete.deepNavy],
+                ),
+                leftChevronIcon:
+                    SvgPicture.asset('assets/icons/arrow-left.svg'),
+                rightChevronIcon:
+                    SvgPicture.asset('assets/icons/arrow-right.svg'),
+              ),
+              calendarBuilders: CalendarBuilders(
+                defaultBuilder: (context, day, focusedDay) {
+                  final value = day.day.toString();
+
+                  return SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Center(
+                      child: Text(
+                        value,
+                        style: types[Types.light_md]!.copyWith(
+                          color: pallete[Pallete.deepNavy],
+                        ),
                       ),
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 0,
-                bottom: 0,
-                left: 0,
-                child: Container(
-                  width: 56,
-                  height: 40,
-                  alignment: Alignment.center,
-                  // clipBehavior: Clip.hardEdge,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: SvgPicture.asset(
-                      'assets/icons/toggle_calendar_btn.svg',
+                  );
+                },
+                todayBuilder: (context, day, focusedDay) {
+                  final value = day.day.toString();
+
+                  return SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Center(
+                      child: Text(
+                        value,
+                        style: types[Types.light_md]!.copyWith(
+                          color: pallete[Pallete.deepNavy],
+                        ),
+                      ),
                     ),
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                  ),
+                  );
+                },
+                selectedBuilder: (context, day, focusedDay) {
+                  final value = day.day.toString();
+                  return Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      decoration: BoxDecoration(
+                        color: pallete[Pallete.deepNavy],
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: Center(
+                        child: Text(value,
+                            style: types[Types.semi_md]!.copyWith(
+                              color: pallete[Pallete.flash],
+                            )),
+                      ),
+                    ),
+                  );
+                },
+                outsideBuilder: (context, day, focusedDay) {
+                  final value = day.day.toString();
+                  return SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Center(
+                      child: Text(value,
+                          style: types[Types.light_md]!.copyWith(
+                            color: pallete[Pallete.alaskanBlue],
+                          )),
+                    ),
+                  );
+                },
+                dowBuilder: (context, day) {
+                  final weekDay = day.weekday;
+                  List<String> list = ['_', 'M', 'T', 'W', 'T', 'F', 'S', 'S'];
+
+                  return SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Center(
+                      child: Text(
+                        list[weekDay],
+                        style: types[Types.semi_md]!.copyWith(
+                          color: pallete[Pallete.deepNavy],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 1,
+          left: 0,
+          child: Column(
+            children: [
+              Container(
+                width: fullWidth,
+                height: 40,
+                color: pallete[Pallete.deepNavy],
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      child: SizedBox(
+                        width: fullWidth,
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            DateFormat('yyyy.MM.dd').format(_selectedDay),
+                            style: types[Types.semi_lg]!.copyWith(
+                              color: pallete[Pallete.white],
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      child: Container(
+                        width: 56,
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset(
+                            'assets/icons/toggle_calendar_btn.svg',
+                          ),
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 57,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 6,
+                        ),
+                        child: VerticalDottedDivider(
+                          color: pallete[Pallete.white],
+                          space: 0.5,
+                          length: 3,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Positioned(
-                top: 0,
-                bottom: 0,
-                left: 57,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 6,
-                  ),
-                  child: VerticalDottedDivider(
-                    color: pallete[Pallete.white],
-                    space: 0.5,
-                    length: 3,
-                  ),
-                ),
-              ),
+              const SizedBox(height: 4),
+              // Container(width: fullWidth, height: 200, color: Colors.red),
             ],
           ),
         ),
-        const SizedBox(height: 16),
-        TableCalendar(
-          firstDay: DateTime(1800),
-          lastDay: DateTime(3000),
-          focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat,
-          daysOfWeekHeight: 50,
-          rowHeight: 50,
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-              _calendarFormat = CalendarFormat.week;
-            });
-          },
-          headerStyle: HeaderStyle(
-            titleCentered: true,
-            formatButtonVisible: false,
-            titleTextFormatter: (date, locale) {
-              return DateFormat.MMMM(locale).format(date);
-            },
-            headerPadding: const EdgeInsets.all(0),
-            headerMargin: const EdgeInsets.only(bottom: 12),
-            titleTextStyle: types[Types.semi_lg]!.copyWith(
-              color: pallete[Pallete.white],
-            ),
-            decoration: BoxDecoration(
-              color: pallete[Pallete.deepNavy],
-            ),
-            leftChevronIcon: SvgPicture.asset('assets/icons/arrow-left.svg'),
-            rightChevronIcon: SvgPicture.asset('assets/icons/arrow-right.svg'),
-          ),
-          calendarBuilders: CalendarBuilders(
-            defaultBuilder: (context, day, focusedDay) {
-              final value = day.day.toString();
-
-              return SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Center(
-                  child: Text(
-                    value,
-                    style: types[Types.light_md]!.copyWith(
-                      color: pallete[Pallete.deepNavy],
-                    ),
-                  ),
-                ),
-              );
-            },
-            todayBuilder: (context, day, focusedDay) {
-              final value = day.day.toString();
-
-              return SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Center(
-                  child: Text(
-                    value,
-                    style: types[Types.light_md]!.copyWith(
-                      color: pallete[Pallete.deepNavy],
-                    ),
-                  ),
-                ),
-              );
-            },
-            selectedBuilder: (context, day, focusedDay) {
-              final value = day.day.toString();
-              return Padding(
-                padding: const EdgeInsets.all(4),
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: pallete[Pallete.deepNavy],
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  child: Center(
-                    child: Text(value,
-                        style: types[Types.semi_md]!.copyWith(
-                          color: pallete[Pallete.flash],
-                        )),
-                  ),
-                ),
-              );
-            },
-            outsideBuilder: (context, day, focusedDay) {
-              final value = day.day.toString();
-              return SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Center(
-                  child: Text(value,
-                      style: types[Types.light_md]!.copyWith(
-                        color: pallete[Pallete.alaskanBlue],
-                      )),
-                ),
-              );
-            },
-            dowBuilder: (context, day) {
-              final weekDay = day.weekday;
-              List<String> list = ['_', 'M', 'T', 'W', 'T', 'F', 'S', 'S'];
-
-              return SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Center(
-                  child: Text(
-                    list[weekDay],
-                    style: types[Types.semi_md]!.copyWith(
-                      color: pallete[Pallete.deepNavy],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-        const SizedBox(height: 16),
       ],
     );
   }
