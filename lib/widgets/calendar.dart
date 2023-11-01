208a828d300ec369c1852ed73765ec008f9db308
taesung1993 <cheonyulin@gmail.com>
2023-11-02 00:42:42 +0900

    refactor calendar and workout_list widgets

--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -6,7 +6,18 @@ import 'package:jeomgymjeok_gwabuha/widgets/year_selector.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends ConsumerStatefulWidget {
-  const Calendar({super.key});
+  const Calendar({
+    super.key,
+    required this.selectedDay,
+    required this.calendarFormat,
+    required this.selectDay,
+    required this.changeYear,
+  });
+
+  final DateTime selectedDay;
+  final CalendarFormat calendarFormat;
+  final void Function(DateTime selectedDay, DateTime focusedDay) selectDay;
+  final void Function(int value) changeYear;
 
   @override
   ConsumerState<Calendar> createState() {
@@ -15,19 +26,6 @@ class Calendar extends ConsumerStatefulWidget {
 }
 
 class _CalendarState extends ConsumerState<Calendar> {
-  CalendarFormat _calendarFormat = CalendarFormat.month;
-
-  void _changeYear(int value) {
-    ref.read(dateProvider.notifier).setYear(value);
-  }
-
-  void selectDay(DateTime selectedDay, DateTime focusedDay) {
-    setState(() {
-      ref.read(dateProvider.notifier).setDate(selectedDay);
-      _calendarFormat = CalendarFormat.week;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     DateTime _selectedDay = ref.watch(dateProvider);
@@ -40,14 +38,14 @@ class _CalendarState extends ConsumerState<Calendar> {
         children: [
           WorkoutCalendar(
             selectedDay: _selectedDay,
-            calendarFormat: _calendarFormat,
-            onDaySelected: selectDay,
+            calendarFormat: widget.calendarFormat,
+            onDaySelected: widget.selectDay,
           ),
           YearSelector(
             selectedDay: _selectedDay,
             start: 2000,
             end: 2100,
-            changeYear: _changeYear,
+            changeYear: widget.changeYear,
           ),
         ],
       ),
--- lib/widgets/workout_list.dart
+++ lib/widgets/workout_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jeomgymjeok_gwabuha/design/Pallete.dart';
+import 'package:jeomgymjeok_gwabuha/design/Types.dart';
 import 'package:jeomgymjeok_gwabuha/models/m_workout_item.dart';
 import 'package:jeomgymjeok_gwabuha/widgets/workout_item/index.dart';
 
@@ -12,14 +14,47 @@ class WorkoutList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListView.separated(
-      itemCount: list.length,
-      itemBuilder: (context, index) => WorkoutItem(
-        workout: list[index],
-      ),
-      separatorBuilder: (context, index) => const SizedBox(
-        height: 2,
+    if (list.isNotEmpty) {
+      return Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 40,
+            color: pallete[Pallete.deepNavy],
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.symmetric(horizontal: 36),
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                '편집',
+                style: types[Types.semi_md]!.copyWith(
+                  color: pallete[Pallete.white],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 4),
+          Expanded(
+            child: ListView.separated(
+              itemCount: list.length,
+              itemBuilder: (context, index) => WorkoutItem(
+                workout: list[index],
+              ),
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 2,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    return Center(
+      child: Text(
+        '근손실 예방이 필요합니다.',
+        style: types[Types.extra_md]!.copyWith(
+          color: pallete[Pallete.deepNavy],
+        ),
       ),
     );
   }
