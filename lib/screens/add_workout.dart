38928eb5cf08f73f11e2a6edfab52987cbb314ec
taesung1993 <cheonyulin@gmail.com>
2023-11-08 11:56:19 +0900

    add the process to valid enteredWorkoutName

--- lib/screens/add_workout.dart
+++ lib/screens/add_workout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jeomgymjeok_gwabuha/design/Pallete.dart';
 import 'package:jeomgymjeok_gwabuha/design/Types.dart';
+import 'package:jeomgymjeok_gwabuha/widgets/common/text_btn.dart';
 import 'package:jeomgymjeok_gwabuha/widgets/date_selector.dart';
 
 class AddWorkout extends StatefulWidget {
@@ -18,6 +19,9 @@ class AddWorkout extends StatefulWidget {
 
 class _AddWorkoutState extends State<AddWorkout> {
   late DateTime _recordDate;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _textController = TextEditingController();
+  bool _invalidWorkoutName = false;
 
   @override
   void initState() {
@@ -55,7 +59,9 @@ class _AddWorkoutState extends State<AddWorkout> {
         ),
         actions: [
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              _formKey.currentState!.validate();
+            },
             child: Text(
               '완료',
               style: types[Types.semi_lg]!.copyWith(
@@ -69,15 +75,93 @@ class _AddWorkoutState extends State<AddWorkout> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(top: 1),
-          child: Stack(
-            children: [
-              DateSelector(
-                selectedDay: _recordDate,
-                start: 2000,
-                end: 2100,
-                changeDate: _changeRecordDate,
-              )
-            ],
+          child: Form(
+            key: _formKey,
+            child: Stack(
+              children: [
+                DateSelector(
+                  selectedDay: _recordDate,
+                  start: 2000,
+                  end: 2100,
+                  changeDate: _changeRecordDate,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 80, left: 24, right: 24),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _textController,
+                        decoration: InputDecoration(
+                          labelText: '운동 이름',
+                          labelStyle: types[Types.semi_md]!.copyWith(
+                              color: _invalidWorkoutName
+                                  ? pallete[Pallete.red01]
+                                  : pallete[Pallete.deepNavy]),
+                          errorStyle: types[Types.input_helper]!
+                              .copyWith(color: pallete[Pallete.red01]),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: pallete[Pallete.deepNavy]!,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: pallete[Pallete.deepNavy]!,
+                              width: 2,
+                            ),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: pallete[Pallete.red01]!,
+                              width: 2,
+                            ),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: pallete[Pallete.red01]!,
+                              width: 2,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 32,
+                            vertical: 16,
+                          ),
+                        ),
+                        keyboardType: TextInputType.text,
+                        style: types[Types.semi_md]!.copyWith(
+                          color: pallete[Pallete.deepNavy],
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            setState(() {
+                              _invalidWorkoutName = true;
+                            });
+                            return '운동명을 입력해 주세요.';
+                          }
+
+                          setState(() {
+                            _invalidWorkoutName = false;
+                          });
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 32),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: TextBtn(
+                          text: '기록 불러오기',
+                          onPressed: () {},
+                          width: 140,
+                          height: 32,
+                          borderRadius: 4,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
